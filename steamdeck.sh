5fc1cf423c4cd11aca591257a05ca4be3b03e4b9
Lee <79258616+wray-lee@users.noreply.github.com>
2023-04-16 21:05:41 +0800

    Update steamdeck.sh

--- steamdeck.sh
+++ steamdeck.sh
@@ -5,7 +5,7 @@ sudo passwd deck
 sudo steamos-readonly disable
 # vibrantDeck
 echo 'Installing vibrantDeck'
-export url='https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_prerelease.sh' && sh -c "$(curl -fsSL $url)"
+export url='https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_prerelease.sh' && sh -c "$(curl -KfsSL $url)"
 echo 'Success'
 
 # Herotic
