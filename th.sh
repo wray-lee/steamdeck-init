a3d6439c303496f516c0c67aa941e1ed73c92725
wray-lee <wray_lee@outlook.com>
2023-04-23 01:40:00 +0800

    Add touhou games startup script

---
+++ th.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+cd '/home/deck/touhou//Touhou Project/'
+ls
+echo ""
+read -p "thgames `printf "\n"`" games
+
+if [ $games==6 ];
+then
+	cd '/home/deck/touhou/Touhou Project/(TH06) Touhou Koumakyou ~ the Embodiment of Scarlet Devil/'
+	wine '/home/deck/touhou/Touhou Project/(TH06) Touhou Koumakyou ~ the Embodiment of Scarlet Devil/th06e.exe'
+elif [ $games==7 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH07) Touhou Youyoumu ~ Perfect Cherry Blossom/'
+	wine '/home/deck/touhou/Touhou Project/(TH07) Touhou Youyoumu ~ Perfect Cherry Blossom/th07e.exe'
+elif [ $games==7.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH07.5) Touhou Suimusou ~ Immaterial and Missing Power/'
+	wine '/home/deck/touhou/Touhou Project/(TH07.5) Touhou Suimusou ~ Immaterial and Missing Power/th075e.exe'
+elif [ $games==8 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH08) Touhou Eiyashou ~ Imperishable Night/'
+	wine '/home/deck/touhou/Touhou Project/(TH08) Touhou Eiyashou ~ Imperishable Night/th08e.exe'
+elif [ $games==9 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH09) Touhou Kaeidzuka ~ Phantasmagoria of Flower View/'
+	wine '/home/deck/touhou/Touhou Project/(TH09) Touhou Kaeidzuka ~ Phantasmagoria of Flower View/th09e.exe'
+elif [ $games==9.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH09.5) Touhou Bunkachou ~ Shoot the Bullet/'
+	wine '/home/deck/touhou/Touhou Project/(TH09.5) Touhou Bunkachou ~ Shoot the Bullet/th095e.exe'
+elif [ $games==10 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH10) Touhou Fuujinroku ~ Mountain of Faith/'
+	wine '/home/deck/touhou/Touhou Project/(TH10) Touhou Fuujinroku ~ Mountain of Faith/th10.exe'
+elif [ $games==10.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH10.5) Touhou Hisouten ~ Scarlet Weather Rhapsody/'
+	wine '/home/deck/touhou/Touhou Project/(TH10.5) Touhou Hisouten ~ Scarlet Weather Rhapsody/th105e.exe'
+elif [ $games==11 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH11) Touhou Chireiden ~ Subterranean Animism/'
+	wine '/home/deck/touhou/Touhou Project/(TH11) Touhou Chireiden ~ Subterranean Animism/th11e.exe'
+elif [ $games==12 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH12) Touhou Seirensen ~ Undefined Fantastic Object'
+	wine '/home/deck/touhou/Touhou Project/(TH12) Touhou Seirensen ~ Undefined Fantastic Object/th12e.exe'
+elif [ $games==12.3 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH12.3) Touhou Hisoutensoku/'
+	wine '/home/deck/touhou/Touhou Project/(TH12.3) Touhou Hisoutensoku/th123e.exe'
+elif [ $games==12.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH12.5) Touhou Bunkachou ~ Double Spoiler/'
+	wine '/home/deck/touhou/Touhou Project/(TH12.5) Touhou Bunkachou ~ Double Spoiler/th125e.exe'
+elif [ $games==12.8 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH12.8) Yousei Daisensou ~ Touhou Sangetsusei/'
+	wine '/home/deck/touhou/Touhou Project/(TH12.8) Yousei Daisensou ~ Touhou Sangetsusei/th128e.exe'
+elif [ $games==13 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH13) Touhou Shinreibyou ~ Ten Desires/'
+	wine '/home/deck/touhou/Touhou Project/(TH13) Touhou Shinreibyou ~ Ten Desires/th13e.exe'
+elif [ $games==13.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH13.5) Touhou Shinkirou ~ Hopeless Masquerade/'
+	wine '/home/deck/touhou/Touhou Project/(TH13.5) Touhou Shinkirou ~ Hopeless Masquerade/th135e.exe'
+elif [ $games==14 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH14) Touhou Kishinjou ~ Double Dealing Character/'
+	wine '/home/deck/touhou/Touhou Project/(TH14) Touhou Kishinjou ~ Double Dealing Character/th14.exe'
+elif [ $games==14.3 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH14.3) Danmaku Amanojaku ~ Impossible Spell Card/'
+	wine '/home/deck/touhou/Touhou Project/(TH14.3) Danmaku Amanojaku ~ Impossible Spell Card/th143.exe'
+elif [ $games==14.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH14.5) Touhou Shinpiroku ~ Urban Legend in Limbo/'
+	wine '/home/deck/touhou/Touhou Project/(TH14.5) Touhou Shinpiroku ~ Urban Legend in Limbo/th145.exe'
+elif [ $games==15 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH15) Touhou Kanjuden ~ Legacy of Lunatic Kingdom/'
+	wine '/home/deck/touhou/Touhou Project/(TH15) Touhou Kanjuden ~ Legacy of Lunatic Kingdom/th15.exe'
+elif [ $games==15.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH15.5) Touhou Hyouibana ~ Antinomy of Common Flowers/'
+	wine '/home/deck/touhou/Touhou Project/(TH15.5) Touhou Hyouibana ~ Antinomy of Common Flowers/th155.exe'
+elif [ $games==16 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH16) Touhou Tenkuushou ~ Hidden Star in Four Seasons/'
+	wine '/home/deck/touhou/Touhou Project/(TH16) Touhou Tenkuushou ~ Hidden Star in Four Seasons/th16.exe'
+elif [ $games==16.5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH16.5) Hifuu Nightmare Diary ~ Violet Detector/'
+	wine '/home/deck/touhou/Touhou Project/(TH16.5) Hifuu Nightmare Diary ~ Violet Detector/th165.exe'
+elif [ $games==17 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH17) Touhou Kikeijuu ~ Wily Beast and Weakest Creature/'
+	wine '/home/deck/touhou/Touhou Project/(TH17) Touhou Kikeijuu ~ Wily Beast and Weakest Creature/th17.exe'
+elif [ $games==18 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH18) Touhou Kouryuudou ~ Unconnected Marketeers/'
+	wine '/home/deck/touhou/Touhou Project/(TH18) Touhou Kouryuudou ~ Unconnected Marketeers/th18.exe'
+elif [ $games==1 ] || [ $games==2 ] || [ $games==3 ] || [ $games==4 ] || [ $games==5 ]
+then
+	cd '/home/deck/touhou/Touhou Project/(TH01-05)/Neko Project II fmgen/'
+	wine '/home/deck/touhou/Touhou Project/(TH01-05)/Neko Project II fmgen/np21nt.exe'
+else
+	echo 'Invaild Number'	
+fi	
+
